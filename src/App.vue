48ad5bf64c521c811c19ff9922d611fd2c505fd9
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-07 11:27:11 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <div class="container">
+    <h1 class="title">Youtube Uygulaması</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+    *{
+      box-sizing: border-box;
+      margin: 0;
+    }
+    .container{
+      max-width: 1200px;
+      width: 100%;
+      margin: 50px auto;
+    }
+    .title{
+      text-align: center;
+    }
+</style>
