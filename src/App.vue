94296de6b82d4637ee2edd14d62cc0f9b17af578
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-07 16:04:37 +0300

    Api Key'e Kadar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  
-  <div class="container">
-    <h1 class="title">Youtube Uygulaması</h1>
+  <div>  
+    
+    <div class="container">
+      <h1 class="title">Youtube Uygulaması</h1>
+      <SearchBar @termChange="onTermChange"/>
+    </div>
   </div>
 </template>
 
 <script>
-
+import SearchBar from './components/SearchBar.vue';
 export default {
   name: 'App',
   components: {
-    
+      SearchBar
+  },
+  methods:{
+      onTermChange(searchTerm){
+          console.log(searchTerm);
+      }
   }
 }
 </script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <form class="searchForm" @submit.prevent="handleSubmit">
+          <input type="text" class="searchInput" v-model="searchTerm">
+          <button class="searchButton">Ara</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    emits:['termChange'],
+    data(){
+      return{
+          searchTerm:''
+      }
+    },
+    methods:{
+      handleSubmit(){
+          this.$emit('termChange',this.searchTerm);
+      }
+    }
+}
+</script>
+
+<style scoped>
+    .searchForm{
+        display:flex;
+        margin:20px 0px;
+    }
+    .searchInput{
+        width:85%;
+        padding: 8px;
+        margin-right: 20px;
+        border-radius: 20px;
+    }
+    .searchButton{
+      width:15%;
+      border-radius: 20px;
+      border:none;
+      background-color: green;
+      color:white;
+      font-size: 18px;
+      cursor:pointer;
+    }
+</style>
